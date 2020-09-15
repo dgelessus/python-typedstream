b2c00df3eea0953457594c94c80b08377b314bc1
dgelessus <dgelessus@users.noreply.github.com>
2020-09-15 22:23:04 +0200

    Remove NSString.__str__
    This way print(nsstring) prints the string's repr and not the unquoted
    contents, which can be confusing.

--- typedstream/classes.py
+++ typedstream/classes.py
@@ -51,9 +51,6 @@ class NSString(NSObject):
 		else:
 			raise ValueError(f"Unsupported version: {archived_class.version}")
 	
-	def __str__(self) -> str:
-		return self.value
-	
 	def __repr__(self) -> str:
 		return f"{type(self).__name__}({self.value!r})"
 
