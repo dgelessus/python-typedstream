51ab46b364a1714496d39a2661080a814131f742
dgelessus <dgelessus@users.noreply.github.com>
2020-09-18 02:14:40 +0200

    Implement NSValue and NSNumber unarchiving

--- typedstream/classes.py
+++ typedstream/classes.py
@@ -1,3 +1,6 @@
+import typing
+
+from . import advanced_repr
 from . import archiver
 
 
@@ -48,4 +51,33 @@ class NSString(NSObject):
 class NSMutableString(NSString):
 	def _init_from_unarchiver_(self, unarchiver: archiver.Unarchiver, class_version: int) -> None:
 		if class_version != 1:
+			raise ValueError(f"Unsupported version: {class_version}")
+
+
+@archiver.archived_class
+class NSValue(NSObject, advanced_repr.AsMultilineStringBase):
+	type_encoding: bytes
+	value: typing.Any
+	
+	def _init_from_unarchiver_(self, unarchiver: archiver.Unarchiver, class_version: int) -> None:
+		if class_version == 0:
+			self.type_encoding = unarchiver.decode_typed_values(b"*")
+			self.value = unarchiver.decode_typed_values(self.type_encoding)
+		else:
+			raise ValueError(f"Unsupported version: {class_version}")
+	
+	def _as_multiline_string_(self, *, state: advanced_repr.RecursiveReprState) -> typing.Iterable[str]:
+		value_it = iter(advanced_repr.as_multiline_string(self.value, calling_self=self, state=state))
+		yield f"{type(self).__name__}, type {self.type_encoding}: " + next(value_it, "")
+		for line in value_it:
+			yield "\t" + line
+	
+	def __repr__(self) -> str:
+		return f"{type(self).__name__}(type_encoding={self.type_encoding!r}, value={self.value!r})"
+
+
+@archiver.archived_class
+class NSNumber(NSValue):
+	def _init_from_unarchiver_(self, unarchiver: archiver.Unarchiver, class_version: int) -> None:
+		if class_version != 0:
 			raise ValueError(f"Unsupported version: {class_version}")
