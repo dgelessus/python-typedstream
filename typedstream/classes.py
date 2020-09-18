69f8e277ecc3d240919ea9cd93416a313f0d32bb
dgelessus <dgelessus@users.noreply.github.com>
2020-09-18 03:16:32 +0200

    Implement NSDictionary and NSMutableDictionary unarchiving

--- typedstream/classes.py
+++ typedstream/classes.py
@@ -1,3 +1,4 @@
+import collections
 import datetime
 import typing
 
@@ -140,6 +141,48 @@ class NSArray(NSObject, advanced_repr.AsMultilineStringBase):
 
 @archiver.archived_class
 class NSMutableArray(NSArray):
+	def _init_from_unarchiver_(self, unarchiver: archiver.Unarchiver, class_version: int) -> None:
+		if class_version != 0:
+			raise ValueError(f"Unsupported version: {class_version}")
+
+
+@archiver.archived_class
+class NSDictionary(NSObject, advanced_repr.AsMultilineStringBase):
+	contents: typing.OrderedDict[typing.Any, typing.Any]
+	
+	def _init_from_unarchiver_(self, unarchiver: archiver.Unarchiver, class_version: int) -> None:
+		if class_version == 0:
+			count = unarchiver.decode_typed_values(b"i")
+			self.contents = collections.OrderedDict()
+			for _ in range(count):
+				key = unarchiver.decode_typed_values(b"@")
+				value = unarchiver.decode_typed_values(b"@")
+				self.contents[key] = value
+		else:
+			raise ValueError(f"Unsupported version: {class_version}")
+	
+	def _as_multiline_string_(self, *, state: advanced_repr.RecursiveReprState) -> typing.Iterable[str]:
+		if not self.contents:
+			count_desc = "empty"
+		elif len(self.contents) == 1:
+			count_desc = "1 entry:"
+		else:
+			count_desc = f"{len(self.contents)} entries:"
+		
+		yield f"{type(self).__name__}, {count_desc}"
+		
+		for key, value in self.contents.items():
+			value_it = iter(advanced_repr.as_multiline_string(value, calling_self=self, state=state))
+			yield f"\t{key!r}: " + next(value_it, "")
+			for line in value_it:
+				yield "\t" + line
+	
+	def __repr__(self) -> str:
+		return f"{type(self).__name__}({self.contents!r})"
+
+
+@archiver.archived_class
+class NSMutableDictionary(NSDictionary):
 	def _init_from_unarchiver_(self, unarchiver: archiver.Unarchiver, class_version: int) -> None:
 		if class_version != 0:
 			raise ValueError(f"Unsupported version: {class_version}")
